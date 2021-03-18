b1f5fc70e1348d4f1742b1494e53056d570f83f9
Talpos Norbert <norberttalpos@gmail.com>
2021-03-18 17:55:28 +0100

    checkGameOver javaslat

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,7 +2,11 @@ package main;
 
 import characters.Settler;
 import interfaces.Steppable;
+import materials.Coal;
+import materials.Iron;
 import materials.Material;
+import materials.Uranium;
+import materials.WaterIce;
 import places.Asteroid;
 import places.AsteroidBelt;
 
@@ -36,19 +40,63 @@ public class Game {
     public boolean CheckGameOver() {
         Map<Asteroid, Map<Class<? extends Material>, Integer>> asteroidMaterialAmounts = new HashMap<>();
 
+        //counting the current materialAmounts of all asteroids in asteroidbelt
         for(Settler settler : settlers){
             var asteroid = settler.GetAsteroid();
-            var materials = settler.GetInventory().GetAmountOfMaterials();
+            var settlerAmounts = settler.GetInventory().GetAmountOfMaterials();
 
             if(!asteroidMaterialAmounts.containsKey(asteroid)){
-                asteroidMaterialAmounts.put(asteroid, materials);
+                asteroidMaterialAmounts.put(asteroid, settlerAmounts);
             }
             else{
                 var alreadyCountedMaterials = asteroidMaterialAmounts.get(asteroid);
 
-                //TODO
+                //merge alreadyCountedMaterials and settlerAmounts
+                for(var materialType : settlerAmounts.keySet()){
+                    Integer settlerAmount = settlerAmounts.get(materialType);
+
+                    if(!alreadyCountedMaterials.containsKey(materialType)){
+                        alreadyCountedMaterials.put(materialType, settlerAmount);
+                    }
+                    else{
+                        Integer alreadyCountedAmount = alreadyCountedMaterials.get(materialType);
+                        alreadyCountedMaterials.put(materialType, settlerAmount + alreadyCountedAmount);
+                    }
+                }
+
+                asteroidMaterialAmounts.put(asteroid, alreadyCountedMaterials);
             }
         }
+
+        //iterate through asteroids, decide if one has enough materials
+
+        Map<Class<? extends Material>, Integer> goalAmount = new HashMap<>();
+        goalAmount.put(Coal.class, 3);
+        goalAmount.put(Iron.class, 3);
+        goalAmount.put(Uranium.class, 3);
+        goalAmount.put(WaterIce.class, 3);
+
+        for(Asteroid asteroid : asteroidMaterialAmounts.keySet()) {
+            var currentAmounts = asteroidMaterialAmounts.get(asteroid);
+
+            boolean hasEnough = true;
+
+            for (var materialType : goalAmount.keySet()) {
+                if (!currentAmounts.containsKey(materialType)) {
+                    hasEnough = false;
+                    break;
+                }
+                if (currentAmounts.get(materialType) < goalAmount.get(materialType)) {
+                    hasEnough = false;
+                    break;
+                }
+            }
+
+            if(hasEnough)
+                return true;
+        }
+
+        return false;
     }
 
    public void AddSteppable(Steppable steppable) {
