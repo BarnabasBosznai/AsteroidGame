0f49bcd34ed0ab594136095201c78d57abcebb60
Barna <bbosznai@gmail.com>
2021-05-02 19:31:37 +0200

    Gombok kicsit atalakitva

--- src/view/Button.java
+++ src/view/Button.java
@@ -3,7 +3,7 @@ package view;
 import java.awt.*;
 
 public class Button implements Clickable {
-    private final int borderX;
+    /*private final int borderX;
     private final int borderY;
     private final int borderWidthX;
     private final int borderWidthY;
@@ -11,13 +11,19 @@ public class Button implements Clickable {
     private final int innerY;
     private final int innerWidthX;
     private final int innerWidthY;
-    private String string;
     private final int stringPosX;
-    private final int stringPosY;
+    private final int stringPosY;*/
+    private String string;
     private final int fontSize;
     private Color backGroundColor;
 
-    public Button(int borderX, int borderY, int borderWidthX, int borderWidthY, int innerX,
+    private Rectangle border;
+    private Rectangle inner;
+    private Font font;
+    private boolean state;
+    private Runnable callback;
+
+    /*public Button(int borderX, int borderY, int borderWidthX, int borderWidthY, int innerX,
                   int innerY, int innerWidthX, int innerWidthY, String string, int stringPosX, int stringPosY, int fontSize){
         this.borderX = borderX;
         this.borderY = borderY;
@@ -32,20 +38,36 @@ public class Button implements Clickable {
         this.stringPosY = stringPosY;
         this.fontSize = fontSize;
         this.backGroundColor = Color.GRAY;
+        this.state = false;
+        this.font = new Font("Dialog",Font.PLAIN,fontSize);
+    }*/
+
+    public Button(Rectangle border, Rectangle inner, String string, int fontSize){
+        this.border = border;
+        this.inner = inner;
+        this.string = string;
+        this.fontSize = fontSize;
+        this.backGroundColor = Color.GRAY;
+        this.state = false;
+        this.font = new Font("Dialog",Font.PLAIN,fontSize);
     }
+
     @Override
     public void Clicked(Position pos, Position cameraPos) {
-
+        this.state = true;
+        if(callback != null)
+            callback.run();
     }
 
     @Override
     public void UnClicked() {
-
+        this.state = false;
     }
 
     @Override
     public boolean ClickedCheck(Position clickPos, Position cameraPos) {
-        return true;
+        //return true;
+        return border.contains(clickPos.x, clickPos.y);
     }
 
     public void SetString(String string){
@@ -56,13 +78,33 @@ public class Button implements Clickable {
         this.backGroundColor = color;
     }
 
+    public boolean getState() {
+        return state;
+    }
+
+    public void setCallback(Runnable runnable) {
+        this.callback = runnable;
+    }
+
+    private void drawCenteredText(Graphics2D g) {
+        FontMetrics metrics = g.getFontMetrics(font);
+        int x = border.x + (border.width - metrics.stringWidth(string)) / 2;
+        int y = border.y + ((border.height - metrics.getHeight()) / 2) + metrics.getAscent();
+        g.drawString(string, x, y);
+    }
+
     public void Draw(Graphics2D graphics){
         graphics.setColor(this.backGroundColor);
-        graphics.fillRect(borderX, borderY, borderWidthX, borderWidthY);
+        //graphics.fillRect(borderX, borderY, borderWidthX, borderWidthY);
+        graphics.fillRect(border.x, border.y, border.width, border.height);
+
         graphics.setColor(Color.LIGHT_GRAY);
-        graphics.fillRect(innerX, innerY, innerWidthX, innerWidthY);
+        //graphics.fillRect(innerX, innerY, innerWidthX, innerWidthY);
+        graphics.fillRect(inner.x, inner.y, inner.width, inner.height);
+
         graphics.setColor(Color.BLACK);
-        graphics.setFont(new Font("Dialog",Font.PLAIN,fontSize));
-        graphics.drawString(string, stringPosX, stringPosY);
+        graphics.setFont(font);
+        drawCenteredText(graphics);
+        //graphics.drawString(string, stringPosX, stringPosY);
     }
 }
--- src/view/InterfacePanel.java
+++ src/view/InterfacePanel.java
@@ -31,7 +31,7 @@ public class InterfacePanel extends Drawable {
         textbox = brown;
 
         this.buttonMap = new HashMap<>();
-        this.buttonMap.put("Robot", new Button(0,434,120,43,5,439,110,33,"Robot",26,466,26));
+        /*this.buttonMap.put("Robot", new Button(0,434,120,43,5,439,110,33,"Robot",26,466,26));
         this.buttonMap.put("CraftTeleport", new Button(0,477,120,43,5,482,110,33,"Teleport",12,508,26));
         this.buttonMap.put("PlaceTeleport", new Button(880,305,120,43,885,310,110,33,"Teleport",893,337,26));
         this.buttonMap.put("Craft", new Button(0,520,120,43,5,525,110,33,"Craft",29,552,26));
@@ -42,7 +42,121 @@ public class InterfacePanel extends Drawable {
         this.buttonMap.put("Coal", new Button(880,348,120,43,885,353,110,33,"Coal",914,380,26));
         this.buttonMap.put("Waterice", new Button(880,434,120,43,885,439,110,33,"Waterice",889,466,26));
         this.buttonMap.put("Uranium", new Button(880,477,120,43,885,482,110,33,"Uranium",892,508,26));
-        this.buttonMap.put("Output", new Button(240,520,520,43,245,525,510,33,output,250,552,29));
+        this.buttonMap.put("Output", new Button(240,520,520,43,245,525,510,33,output,250,552,29));*/
+
+        this.buttonMap.put("Robot", new Button(new Rectangle(0,434,120,43), new Rectangle(5,439,110,33),"Robot",26));
+        this.buttonMap.put("CraftTeleport", new Button(new Rectangle(0,477,120,43), new Rectangle(5,482,110,33),"Teleport",26));
+        this.buttonMap.put("PlaceTeleport", new Button(new Rectangle(880,305,120,43), new Rectangle(885,310,110,33),"Teleport",26));
+        this.buttonMap.put("Craft", new Button(new Rectangle(0,520,120,43), new Rectangle(5,525,110,33),"Craft",26));
+        this.buttonMap.put("Drill", new Button(new Rectangle(120,520,120,43), new Rectangle(125,525,110,33),"Drill",26));
+        this.buttonMap.put("Mine", new Button(new Rectangle(760,520,120,43), new Rectangle(765,525,110,33),"Mine",26));
+        this.buttonMap.put("Place", new Button(new Rectangle(880,520,120,43), new Rectangle(885,525,110,33),"Place",26));
+        this.buttonMap.put("Iron", new Button(new Rectangle(880,391,120,43), new Rectangle(885,396,110,33),"Iron",26));
+        this.buttonMap.put("Coal", new Button(new Rectangle(880,348,120,43), new Rectangle(885,353,110,33),"Coal",26));
+        this.buttonMap.put("Waterice", new Button(new Rectangle(880,434,120,43), new Rectangle(885,439,110,33),"Waterice",26));
+        this.buttonMap.put("Uranium", new Button(new Rectangle(880,477,120,43), new Rectangle(885,482,110,33),"Uranium",26));
+        this.buttonMap.put("Output", new Button(new Rectangle(240,520,520,43), new Rectangle(245,525,510,33),output,29));
+
+        this.buttonMap.get("Drill").setCallback(() -> {
+            if (waitingSettler.Drill()){
+                output = "Sikerült fúrni!";
+                stepped = true;
+                Controller.getInstance().SettlerStepped();
+            } else {
+                output = "Nem sikerült fúrni!";
+            }
+        });
+        this.buttonMap.get("Mine").setCallback(() -> {
+            if (waitingSettler.Mine()){
+                output = "Sikerült bányászni!";
+                stepped = true;
+                Controller.getInstance().SettlerStepped();
+            } else {
+                output = "Nem sikerült bányászni!";
+            }
+        });
+        this.buttonMap.get("Robot").setCallback(() -> {
+            if(buttonMap.get("Craft").getState()) {
+                if (waitingSettler.CraftRobot()) {
+                    output = "Sikerült a Robot craftolása!";
+                    stepped = true;
+                    Controller.getInstance().SettlerStepped();
+                } else {
+                    output = "Nem sikerült a Robot Craftolása!";
+                }
+            }
+        });
+        this.buttonMap.get("CraftTeleport").setCallback(() -> {
+            if(buttonMap.get("Craft").getState()) {
+                if (waitingSettler.CraftTeleportGates()) {
+                    output = "Sikerült a Teleport craftolása!";
+                    stepped = true;
+                    Controller.getInstance().SettlerStepped();
+                } else {
+                    output = "Nem sikerült a Teleport Craftolása!";
+                }
+            }
+        });
+        this.buttonMap.get("Iron").setCallback(() -> {
+            if(this.buttonMap.get("Place").getState()) {
+                if (waitingSettler.PlaceMaterial(new Iron())) {
+                    output = "Sikerült letenni a Vasat!";
+                    stepped=true;
+                    Controller.getInstance().SettlerStepped();
+                } else {
+                    output = "Nem sikerült letenni a Vasat!";
+                }
+            }
+        });
+        this.buttonMap.get("Coal").setCallback(() -> {
+            if(this.buttonMap.get("Place").getState()) {
+                if (waitingSettler.PlaceMaterial(new Coal())) {
+                    output = "Sikerült letenni a Szenet!";
+                    stepped=true;
+                    Controller.getInstance().SettlerStepped();
+                } else {
+                    output = "Nem sikerült letenni a Szenet!";
+                }
+            }
+        });
+        this.buttonMap.get("Waterice").setCallback(() -> {
+            if(this.buttonMap.get("Place").getState()) {
+                if (waitingSettler.PlaceMaterial(new WaterIce())) {
+                    output = "Sikerült letenni a Vízjeget!";
+                    stepped=true;
+                    Controller.getInstance().SettlerStepped();
+                } else {
+                    output = "Nem sikerült letenni a Vízjeget!";
+                }
+            }
+        });
+        this.buttonMap.get("Uranium").setCallback(() -> {
+            if(this.buttonMap.get("Place").getState()) {
+                if (waitingSettler.PlaceMaterial(new Uranium())) {
+                    output = "Sikerült letenni az Uránt!";
+                    stepped=true;
+                    Controller.getInstance().SettlerStepped();
+                } else {
+                    output = "Nem sikerült letenni az Uránt!";
+                }
+            }
+        });
+        this.buttonMap.get("PlaceTeleport").setCallback(() -> {
+            if(this.buttonMap.get("Place").getState()) {
+                if (waitingSettler.PlaceTeleportGate()) {
+                    output = "Sikerült letenni a Teleportkaput!";
+                    stepped = true;
+                    Controller.getInstance().SettlerStepped();
+                } else {
+                    output = "Nem sikerült letenni a Teleportkaput!";
+                }
+            }
+        });
+        this.buttonMap.get("Output").setCallback(() -> {
+            // :woozy_face:
+            output = "Itt meg mit szeretnél?";
+        });
+
 
         try{
             //Beolvasas utan automatikusan bezarodnak a fajlok az ImageIO-nal
@@ -58,8 +172,7 @@ public class InterfacePanel extends Drawable {
 
     @Override
     public void Draw(Graphics2D graphics, Position cameraPos) { // boolean-eket itt nagyban használnám
-        if (craft){ // mágikus számok hada 1.
-
+        if (buttonMap.get("Craft").getState()){ // mágikus számok hada 1.
             // Robot craftoló gomb
             this.buttonMap.get("Robot").Draw(graphics);
 
@@ -75,7 +188,7 @@ public class InterfacePanel extends Drawable {
             this.buttonMap.get("Craft").Draw(graphics);
         }
 
-        if (place){ // mágikus számok hada 3.
+        if (buttonMap.get("Place").getState()) { // mágikus számok hada 3.
 
             /// Teleportkaput letevő gomb
             this.buttonMap.get("PlaceTeleport").Draw(graphics);
@@ -186,7 +299,22 @@ public class InterfacePanel extends Drawable {
         }
         boolean clickedOnInterface = false;
 
-        if (craft){
+        var res = buttonMap.entrySet().stream()
+                .filter(stringButtonEntry -> stringButtonEntry.getValue().ClickedCheck(clickPos, null)).findAny();
+        if(res.isPresent()) {
+            res.get().getValue().Clicked(clickPos, null);
+            if(buttonMap.get("Place").getState() && buttonMap.get("Craft").getState())
+                clickedOnInterface = true;
+
+            buttonMap.entrySet().forEach(stringButtonEntry -> {
+                if(!stringButtonEntry.equals(res.get()))
+                    stringButtonEntry.getValue().UnClicked();
+            });
+        } else {
+            buttonMap.forEach((s, button) -> button.UnClicked());
+        }
+
+        /*if (craft){
             if (clickPos.x<120 && clickPos.y>434){
                 clickedOnInterface = true;
                 if (clickPos.y>434 && clickPos.y<477) {
@@ -211,9 +339,9 @@ public class InterfacePanel extends Drawable {
                     craft = false;
                 }
             }
-        }
+        }*/
 
-        if (place){
+        /*if (place){
             if (clickPos.x>880 && clickPos.y>305){
                 clickedOnInterface = true;
                 if (clickPos.y>305 && clickPos.y<348) {
@@ -267,10 +395,10 @@ public class InterfacePanel extends Drawable {
             }
         }
 
-        craft = false; place = false; allMaterial = false;
+        craft = false; place = false; allMaterial = false;*/
         //lekezelni, hogy volt e valamelyik gombra/akarmire kattintas clickPos alapjan
 
-        if (clickPos.y>520){
+        /*if (clickPos.y>520){
             clickedOnInterface=true;
             if (clickPos.x<120)
                 craft=true;
@@ -295,12 +423,13 @@ public class InterfacePanel extends Drawable {
             } else {
                 place=true;
             }
-        }
+        }*/
 
-        if (clickPos.y<43){
+        /*if (clickPos.y<43){
             if (clickPos.x>240 && clickPos.y<760)
                 clickedOnInterface=true;
-        }
+        }*/
+
         return !clickedOnInterface;
     }
 }
